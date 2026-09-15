556d677062a0575f68121c9596adfcb9ecb84ac1
Blickdoktor <kontakt@blickdoktor.de>
2026-09-15 11:32:51 +0000

    Vincents Foto auf Startseite: Skalierung 1.15 + nach oben verschoben
    Kopfhöhe jetzt näher an Evrims Foto angepasst, Kragen bleibt sichtbar.

--- Doctors.jsx
+++ Doctors.jsx
@@ -23,7 +23,7 @@ function Doctors() {
       href: 'aerzte/dr-vincent-oehmichen/',
       photo: 'dr-vincent-oehmichen.jpg',
       photoPosition: 'center top',
-      photoScale: 1.08
+      photoScale: 1.15
     },
   ];
 
@@ -54,7 +54,7 @@ function Doctors() {
                   marginBottom: 16, overflow: 'hidden'
                 }}>
                   {d.photo
-                    ? <img src={d.photo} alt={d.name} style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: d.photoPosition || 'center', transform: d.photoScale ? `scale(${d.photoScale})` : undefined, transformOrigin: 'center 15%', display: 'block' }} />
+                    ? <img src={d.photo} alt={d.name} style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: d.photoPosition || 'center', transform: d.photoScale ? `scale(${d.photoScale}) translateY(-6%)` : undefined, display: 'block' }} />
                     : d.initials
                   }
                 </div>
